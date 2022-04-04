606bf1d5fefb104ae882fde1bd46f7277fef36ce
Vibhav Gopal <vibhavgopal2004@gmail.com>
2022-04-04 23:44:56 +0530

    incomplete... battery low, my energy low

--- person/index.js
+++ person/index.js
@@ -1,30 +1,5 @@
 
-function init(){
-// Import the functions you need from the SDKs you need
-;
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBF_YeGI5gwab62e5lTuGcLspT3bmJM4rM",
-  authDomain: "gdsc-storage-5b8ab.firebaseapp.com",
-  databaseURL: "https://gdsc-storage-5b8ab-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "gdsc-storage-5b8ab",
-  storageBucket: "gdsc-storage-5b8ab.appspot.com",
-  messagingSenderId: "694473698039",
-  appId: "1:694473698039:web:507a351f4993d66b9981f2",
-  measurementId: "G-RRWSRBENWV"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-
-
-
-}
-init()
 
 function writeUserData(userId, name, email, imageUrl) {
   try {
