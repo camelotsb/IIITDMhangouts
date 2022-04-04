d9b16d147ecbb572ff9aa0f1b70f852fb48623cb
Vibhav Gopal <vibhavgopal2004@gmail.com>
2022-04-04 20:10:54 +0530

    small changes

--- person/index.js
+++ person/index.js
@@ -26,8 +26,10 @@ const database = getDatabase(app);
 
 
 }
+init()
 
 function writeUserData(userId, name, email, imageUrl) {
+
     const db = getDatabase();
     set(ref(db, 'users/' + userId), {
       username: name,
